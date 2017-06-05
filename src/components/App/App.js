03f022fa5f79261fe71a3124ac403b31ea4e7c4b
Enzo Frantz <enzo.frantz@goeuro.com>
2017-06-05 18:16:36 +0200

    add styled components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,24 @@
+import styled from 'styled-components';
 import React from 'react';
 import helloWorld from '../../modules/hello.module';
+
+const Wrapper = styled.div`
+  padding: 5px;
+  background: linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%);
+`;
+
+const Title = styled.h1`
+  font-size: 22px;
+  text-align: center;
+  color: white;
+  font-family: Arial;
+`;
 
 export default function () {
   const result = helloWorld();
   return (
-    <div>
-      {result}
-    </div>
+    <Wrapper>
+      <Title>{result}</Title>
+    </Wrapper>
   );
 }
--- src/modules/hello.module.js
+++ src/modules/hello.module.js
@@ -1,3 +1,3 @@
 export default function () {
-  return 'hello world u wot';
+  return 'Hello World.';
 }
